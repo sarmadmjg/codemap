9bba1107f69cf9d1ce1781fb6d13080ce71e081a
Sarmad Georgie <sarmadmjg@gmail.com>
2017-08-03 17:11:33 +0300

    Implement the API

--- api/index.js
+++ api/index.js
@@ -1,9 +1,39 @@
 let express = require('express');
-
 let router = express.Router();
+
+const db = require('../db_setup');
+const Category = db.Category;
+const User = db.User;
+const Entry = db.Entry;
 
 router.get('/', (req, res) => {
     res.end('You reached the api');
-})
+});
+
+router.get('/categories', (req, res) => {
+    Category.findAll().then(cats => {
+        res.json({categories: cats});
+    });
+});
+
+router.get('/categories/:cat/entries', (req, res) => {
+    Entry.findAll({
+        where: {
+            categoryName: req.params.cat
+        }
+    }).then(entries => {
+        res.json({entries: entries});
+    });
+});
+
+router.get('/entries/:id', (req, res) => {
+    Entry.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(entry => {
+        res.json(entry);
+    })
+});
 
 module.exports = router;
